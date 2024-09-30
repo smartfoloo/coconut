bd83f736d27344f7e1fac67090caee8efa60ece1
platformerdotio <smartfoloo@gmail.com>
2024-09-30 00:30:33 -0500

    update port in app.js

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.get('/search', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
