7453654a3d7949e2d86300ad0e7d1dca7fcfd1fa
smxrtfoloo <smartfoloo@gmail.com>
2024-10-08 18:26:32 -0500

    offline redirect fix

--- public/js/sw.js
+++ public/js/sw.js
@@ -13,6 +13,13 @@ self.addEventListener('install', event => {
       ]);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    clients.claim()
+  );
 });
 
 self.addEventListener('fetch', event => {
